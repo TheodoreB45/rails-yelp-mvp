fa40dc8e380f31a3fa9d7d56bbd8d78b91c5ee9c
Theodore Bischoff <theodorebb5@gmail.com>
2024-11-14 11:14:43 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, except: [:edit, :update, :destroy]
+  resources :reviews, except: [:edit, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database"
+Restaurant.destroy_all
+puts "Creating restaurants"
+Restaurant.create!(name: "Zafferano", address: "Eaton Square", phone_number: "07748287766", category: "italian")
+puts "Created Zafferano"
+Restaurant.create!(name: "Entrecote", address: "Somewhere", phone_number: "07748287767", category: "french")
+puts "Created Entrecote"
+Restaurant.create!(name: "Sumi", address: "Near home", phone_number: "07748287768", category: "japanese")
+puts "Created Sumi"
+Restaurant.create!(name: "Eat Tokyo", address: "Notting Hill Gate", phone_number: "07748287769", category: "japanese")
+puts "Created Eat Tokyo"
+Restaurant.create!(name: "Itsu", address: "Anywhere", phone_number: "07748287765", category: "japanese")
+puts "Created Itsu"
